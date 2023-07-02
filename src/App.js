9eaebcb1e6788d0f2461012fcb6e2a2cef0ca874
Laksumi Soma <92606166+sumi2202@users.noreply.github.com>
2023-07-02 14:38:39 -0400

    -

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import Navbar from './components/Navbar';
+import React from "react";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
@@ -7,6 +7,6 @@ function App() {
       <Navbar />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React from "react";
 
 const Navbar = () => {
     return(
-        <div>Navbar</div>
-    )
-}
+        <div className = "flex justify-between text-white">
+            <h1 className="w-full text-3xl font-bold text-[#00df9a]">REACT.</h1>
+            <ul className="flex">
+                <li className ="p-4">Home</li>
+                <li className ="p-4">About</li>
+                <li className ="p-4">Contact</li>
+            </ul>
+        </div>
+    );
+};
 
-export default Navbar
+export default Navbar;

a77d2ae03cf96349e11378f9cb29a02ecb761c91
Laksumi Soma <92606166+sumi2202@users.noreply.github.com>
2023-07-02 15:59:18 -0400

    navbar fixes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
+import MainPageHero from "./components/MainPageHero";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <MainPageHero/>
+
     </div>
   );
 };

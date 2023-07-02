c719ebe935e5345347c5609d8173caacb4e3a21f
Laksumi Soma <92606166+sumi2202@users.noreply.github.com>
2023-07-02 19:18:19 -0400

    mainpage first section

--- src/components/MainPageHero.jsx
+++ src/components/MainPageHero.jsx
@@ -2,8 +2,13 @@ import React from "react";
 
 const MainPageHero = () => {
     return (
-        <div>
-
+        <div className="text-white">
+            <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
+            <h2>Welcome to My E-Portfolio</h2>
+            <div>
+                <h1>Get to know me. I am:</h1>
+            </div>
+            </div>
         </div>
     );
 };

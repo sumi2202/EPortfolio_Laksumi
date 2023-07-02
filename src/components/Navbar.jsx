5f1d31c7d61de5a223e2214b54a770f734a9f82a
Laksumi Soma <92606166+sumi2202@users.noreply.github.com>
2023-07-02 15:00:40 -0400

    -

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navbar = () => {
     return(
-        <div className ="flex justify-between text-white">
+        <div className ="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
             <h1 className="w-full text-3xl font-bold text-[#00df9a]">REACT.</h1>
             <ul className="flex">
                 <li className ="p-4">Home</li>

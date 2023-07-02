fec737c3c4f218609b31a0712f1d014df3e1a1e3
Laksumi Soma <92606166+sumi2202@users.noreply.github.com>
2023-07-02 15:06:37 -0400

    -

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,10 +3,11 @@ import React from "react";
 const Navbar = () => {
     return(
         <div className ="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
-            <h1 className="w-full text-3xl font-bold text-[#00df9a]">REACT.</h1>
+            <h1 className="w-full text-3xl font-bold text-[#00df9a]">Laksumi Soma.</h1>
             <ul className="flex">
                 <li className ="p-4">Home</li>
                 <li className ="p-4">About</li>
+                <li className ="p-4">Projects</li>
                 <li className ="p-4">Contact</li>
             </ul>
         </div>

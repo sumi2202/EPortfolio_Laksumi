cf6e61c1c641dbd1a41ed0f55245370735ab9d82
Laksumi Soma <92606166+sumi2202@users.noreply.github.com>
2023-07-02 15:52:16 -0400

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,10 +16,10 @@ const Navbar = () => {
                 <li className ="p-4">Projects</li>
                 <li className ="p-4">Contact</li>
             </ul>
-            <div>
+            <div onClick={handleNav} className="block md:hidden">
                 {!nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20} />}
             </div>
-            <div className="fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300]">
+            <div className={!nav ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500" : "fixed left-[-100%]"}>
             <h1 className="w-full text-3xl font-bold text-[#00df9a] m-4">Laksumi Soma.</h1>
                 <ul className="uppercase p-4">
                     <li className ="p-4 border-b border-gray-600">Home</li>

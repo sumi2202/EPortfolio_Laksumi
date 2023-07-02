609d4d90de8d51ef4eda08e9ff40bd7304c79158
Laksumi Soma <92606166+sumi2202@users.noreply.github.com>
2023-07-02 15:27:40 -0400

    --

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import {AiOutlineClose, AiOutlineMenu} from "react-icons/ai"
 
 const Navbar = () => {
+    const [nav, setNav] = useState(false);
+
+    const handleNav = () => {
+        setNav(!nav)
+    };
     return(
         <div className ="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
             <h1 className="w-full text-3xl font-bold text-[#00df9a]">Laksumi Soma.</h1>
@@ -12,11 +17,11 @@ const Navbar = () => {
                 <li className ="p-4">Contact</li>
             </ul>
             <div>
-                <AiOutlineMenu size={20} />
+                {!nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20} />}
             </div>
             <div className="fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300]">
             <h1 className="w-full text-3xl font-bold text-[#00df9a] m-4">Laksumi Soma.</h1>
-                <ul className="uppercase">
+                <ul className="uppercase p-4">
                     <li className ="p-4 border-b border-gray-600">Home</li>
                     <li className ="p-4 border-b border-gray-600">About</li>
                     <li className ="p-4 border-b border-gray-600">Projects</li>

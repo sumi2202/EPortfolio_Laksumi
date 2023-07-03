fcc62e06128ec6ac45654b8b2519d83593f90eb0
Laksumi Soma <92606166+sumi2202@users.noreply.github.com>
2023-07-02 20:31:24 -0400

    styling

--- src/components/MainPageHero.jsx
+++ src/components/MainPageHero.jsx
@@ -6,9 +6,10 @@ const MainPageHero = () => {
         <div className="text-white">
             <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
             <h2 className="md:text-7x1 sm:text-6xl text-4xl text-white font-bold md:py-6">Welcome to My E-Portfolio</h2>
-            <div>
-                <h1 className="text-[#00df9a] md:text-4xl sm:tex-3xl text-xl font-bold">Get to know me. I am:</h1>
+            <div className="flex justify-center items-center">
+                <h1 className="text-[#00df9a] md:text-4xl sm:tex-3xl text-xl font-bold">Get to know me. I am</h1>
                 <Typed 
+                className="text-white md:text-4xl sm:tex-3xl text-xl font-bold pl-2"
                 strings={['Creative', 'Inquisitive', 'Ambitious']} 
                 typeSpeed={120} 
                 backspeed={140} 

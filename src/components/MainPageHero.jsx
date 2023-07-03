9f8831c5e10270457176e80fae7c276c941719fd
Laksumi Soma <92606166+sumi2202@users.noreply.github.com>
2023-07-02 22:30:07 -0400

    more styling fun

--- src/components/MainPageHero.jsx
+++ src/components/MainPageHero.jsx
@@ -3,16 +3,26 @@ import Typed from "react-typed";
 
 const MainPageHero = () => {
     return (
-        <div className="text-white">
+        <div className="text-[#fbf5f3]">
             <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
-            <h2 className="md:text-7x1 sm:text-6xl text-4xl text-white font-bold md:py-6">Welcome to My E-Portfolio</h2>
+            <h2 className="md:text-7x1 sm:text-6xl text-4xl text-[#fbf5f3] font-bold md:py-6">Welcome to My E-Portfolio</h2>
             <div className="flex justify-center items-center">
-                <h1 className="text-[#00df9a] md:text-4xl sm:tex-3xl text-xl font-bold">Get to know me. I am</h1>
+                <h1 className="text-[#fbf5f3] md:text-4xl sm:tex-3xl text-xl font-bold">Get to know me. I am</h1>
                 <Typed 
-                className="text-white md:text-4xl sm:tex-3xl text-xl font-bold pl-2"
-                strings={['Creative', 'Inquisitive', 'Ambitious']} 
+                className="text-[#f72585] md:text-4xl sm:tex-3xl text-xl font-bold pl-2"
+                strings=
+                {['adventurous.',
+                'ambitious.',
+                'charismatic.',
+                'creative.',
+                'dedicated.',
+                'determined.',
+                'empathetic.',
+                'easy-going.',
+                'imaginative.',
+                'inquisitive.']} 
                 typeSpeed={120} 
-                backspeed={140} 
+                backspeed={160} 
                 loop
                 />
             </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,8 +8,8 @@ const Navbar = () => {
         setNav(!nav)
     };
     return(
-        <div className ="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
-            <h1 className="w-full text-3xl font-bold text-[#00df9a]">Laksumi Soma.</h1>
+        <div className ="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-[#fbf5f3] font-bold">
+            <h1 className="w-full text-3xl font-bold text-[#f72585]">Laksumi Soma.</h1>
             <ul className="hidden md:flex">
                 <li className ="p-4">Home</li>
                 <li className ="p-4">About</li>
@@ -19,13 +19,13 @@ const Navbar = () => {
             <div onClick={handleNav} className="block md:hidden">
                 {!nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20} />}
             </div>
-            <div className={!nav ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500" : "fixed left-[-100%]"}>
-            <h1 className="w-full text-3xl font-bold text-[#00df9a] m-4">Laksumi Soma.</h1>
+            <div className={!nav ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-[#7209b7] bg-[#02020a] ease-in-out duration-500" : "fixed left-[-100%]"}>
+            <h1 className="w-full text-3xl font-bold text-[#f72585] m-4">Laksumi Soma.</h1>
                 <ul className="uppercase p-4">
-                    <li className ="p-4 border-b border-gray-600">Home</li>
-                    <li className ="p-4 border-b border-gray-600">About</li>
-                    <li className ="p-4 border-b border-gray-600">Projects</li>
-                    <li className ="p-4 border-b border-gray-600">Contact</li>
+                    <li className ="p-4 text-[#fbf5f3] font-bold border-b border-[#f72585]">Home</li>
+                    <li className ="p-4 text-[#fbf5f3] font-bold border-b border-[#f72585]">About</li>
+                    <li className ="p-4 text-[#fbf5f3] font-bold border-b border-[#f72585]">Projects</li>
+                    <li className ="p-4 text-[#fbf5f3] font-bold border-b border-[#f72585]">Contact</li>
                 </ul>
             </div>
         </div>

38c380f6b7f0851c0a02b721ef6ad80e16948177
Laksumi Soma <92606166+sumi2202@users.noreply.github.com>
2023-07-04 22:46:01 -0400

    --

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import MainPageHero from "./components/MainPageHero";
 import Navbar from "./components/Navbar";
+import About from "./components/About";
 
 function App() {
   return (
     <div>
       <Navbar />
       <MainPageHero/>
+      <About />
 
     </div>
   );
